10077dbb36e9d39b080249eb63eac4256fe5ed81
lixiang <lixiang2014@xiaochuankeji.cn>
2021-08-03 19:32:45 +0800

    ADD two sum

---
+++ README.md
@@ -0,0 +1,1 @@
+LeetCode Go版本
---
+++ leetcode/0001.Two-Sum/twoSum.go
@@ -0,0 +1,15 @@
+package leetcode
+
+/**
+题目链接:https://leetcode-cn.com/problems/two-sum/
+*/
+func twoSum(nums []int, target int) []int {
+	sumMap := make(map[int]int)
+	for i, v := range nums {
+		if k, ok := sumMap[target-v]; ok {
+			return []int{k, i}
+		}
+		sumMap[v] = i
+	}
+	return nil
+}
