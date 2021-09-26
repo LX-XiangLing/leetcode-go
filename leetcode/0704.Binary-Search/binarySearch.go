23d32c4be96eb1705a98fec92241842fa726cd3e
lixiang <lixiang2014@xiaochuankeji.cn>
2021-09-26 14:59:43 +0800

    add binary search

---
+++ leetcode/0704.Binary-Search/binarySearch.go
@@ -0,0 +1,23 @@
+package _704_Binary_Search
+
+/**
+题目链接:https://leetcode-cn.com/problems/binary-search/
+*/
+
+func search(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		index := (high-low)/2 + low
+		num := nums[index]
+		if num == target {
+			return index
+		}
+		if num > target {
+			high = index - 1
+		}
+		if num < target {
+			low = index + 1
+		}
+	}
+	return -1
+}
