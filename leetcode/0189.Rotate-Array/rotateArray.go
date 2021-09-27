dcf352f96031aa4994006a3f4bf979fd0423a1ac
lixiang <lixiang2014@xiaochuankeji.cn>
2021-09-27 21:03:06 +0800

    add 旋转数组

---
+++ leetcode/0189.Rotate-Array/rotateArray.go
@@ -0,0 +1,17 @@
+package _189_Rotate_Array
+
+/**
+ * 题目链接: https://leetcode-cn.com/problems/rotate-array/
+ */
+func rotate(nums []int, k int) {
+	k %= len(nums)
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+}
+
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
